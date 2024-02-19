a87f269efe4ffbd2a2f0c833b6d7e9c7320ed281
rohitkhirid <rohitkhirid@gmail.com>
2024-02-19 15:53:14 +0530

    chore: fixed core sample app

--- iOS-core/DyteCoreExample/Utils/Constants.swift
+++ iOS-core/DyteCoreExample/Utils/Constants.swift
@@ -10,11 +10,6 @@ import UIKit
 import UIKit
 
 struct Constants {
-   
-    static let AUTH_TOKEN = YOUR_AUTH_TOKEN_HERE
-
-    static let BASE_URL = "https://api.cluster.dyte.in/v2"
-    
     static let UUID = UIDevice.current.identifierForVendor?.uuidString ?? ""
     static let errorLoadingImage = "Error Loading Image!"
     static let errorTitle = "Error!"
