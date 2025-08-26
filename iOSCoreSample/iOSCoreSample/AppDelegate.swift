e932b31db5ed0742772e8aa01c903d57d8404f5c
Yash Garg <ygarg@cloudflare.com>
2025-08-26 12:49:46 +0530

    chore: cleanup app delegate pushkit

--- iOSCoreSample/iOSCoreSample/AppDelegate.swift
+++ iOSCoreSample/iOSCoreSample/AppDelegate.swift
@@ -1,23 +1,10 @@
-import PushKit
-import RealtimeKit
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
     var window: UIWindow?
-    var meeting: RealtimeKitClient!
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let pushRegistry = PKPushRegistry(queue: DispatchQueue.main)
-        pushRegistry.delegate = self
-        pushRegistry.desiredPushTypes = [.voIP]
-
         return true
     }
-
-    func pushRegistry(_: PKPushRegistry, didUpdate _: PKPushCredentials, for _: PKPushType) {
-        // Send the push credentials to your server
-    }
-
-    func pushRegistry(_: PKPushRegistry, didReceiveIncomingPushWith _: PKPushPayload, for _: PKPushType, completion _: @escaping () -> Void) {}
 }
